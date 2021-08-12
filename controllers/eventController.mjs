ec93ce8c7dc5cc359557785fd40f9f6ad4e2db8a
Derick Yearnsmith <d.yearnsmith@gmail.com>
2021-08-12 23:40:44 +1000

    fix bug: null goalsId preventing save of event with no goal

--- controllers/eventController.mjs
+++ controllers/eventController.mjs
@@ -14,16 +14,20 @@ const errorHandling = function(res, err, code) {
 const newEvent = function (req, res){
     addUserEvent(req).save()
         .then(newEvent => {
-            Event.populate(newEvent,{path:'goalsId', select:{title:1}})
-            .then(nE => {
-                //loop through the attached goals for newEvent and the event Id
-                // each goal's eventsId field.
-                nE.goalsId.forEach( goalId =>{
+
+            //loop through the attached goals for newEvent and the event Id
+            // each goal's eventsId field.
+            if(newEvent.length > 0){
+                console.log(newEvent.goalsId)
+                newEvent.goalsId.forEach( goalId =>{
                     Goal.findByIdAndUpdate(goalId._id,
                         {$push: {eventsId: nE._id}},
                         {safe: true, upsert: true},
                         function(err) { if(err){ console.log(err) } })
                 })
+            }
+            Event.populate(newEvent,{path:'goalsId', select:{title:1}})
+            .then(nE => {
                 res.send(nE)
             })
         })
